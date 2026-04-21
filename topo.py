46ce3b80586d9c087f26ff330f7b8d8181e4dfce
Erwann Charlier <erwann.charlier@outlook.be>
2026-04-21 14:30:20 +0200

    init

---
+++ topo.py
@@ -0,0 +1,216 @@
+"""
+Custom topology for the LINFO2347 - Computer System Security course.
+
+Mimics a small enterprise network, with 2 workstations in a trusted LAN,
+and service-providing servers in a DMZ LAN.
+
+Adapted from https://stackoverflow.com/questions/46595423/mininet-how-to-create-a-topology-with-two-routers-and-their-respective-hosts
+"""
+
+import os
+import time
+import argparse
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.examples.linuxrouter import LinuxRouter
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+
+class TopoSecu(Topo):
+    """
+    Custom Mininet topology
+    """
+
+    def build(self):
+
+        # Add 2 routers in two different subnets
+        r1 = self.addHost('r1', cls=LinuxRouter, ip=None)  # Workstation LAN router
+        r2 = self.addHost('r2', cls=LinuxRouter, ip=None)  # Internet router
+
+        # Add 2 switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        # Add router-switch links in the same subnet
+        self.addLink(s1, r1, intfName2='r1-eth0', params2={'ip': '10.1.0.1/24'})
+        self.addLink(s2, r1, intfName2='r1-eth12', params2={'ip': '10.12.0.1/24'})
+        self.addLink(s2, r2, intfName2='r2-eth12', params2={'ip': '10.12.0.2/24'})
+
+        # Add outside host
+        internet = self.addHost(name='internet', ip='10.2.0.2/24', defaultRoute='via 10.2.0.1')
+        self.addLink(internet, r2, intfName2='r2-eth0', params2={'ip': '10.2.0.1/24'})
+
+        # Adding hosts specifying the default route
+        ws2 = self.addHost(name='ws2', ip='10.1.0.2/24', defaultRoute='via 10.1.0.1')
+        ws3 = self.addHost(name='ws3', ip='10.1.0.3/24', defaultRoute='via 10.1.0.1')
+        httpServer = self.addHost(name='http', ip='10.12.0.10/24', defaultRoute='via 10.12.0.2')
+        dnsServer = self.addHost(name='dns', ip='10.12.0.20/24', defaultRoute='via 10.12.0.2')
+        ntpServer = self.addHost(name='ntp', ip='10.12.0.30/24', defaultRoute='via 10.12.0.2')
+        ftpServer = self.addHost(name='ftp', ip='10.12.0.40/24', defaultRoute='via 10.12.0.2')
+
+        # Add host-switch links
+        self.addLink(ws2, s1)
+        self.addLink(ws3, s1)
+        self.addLink(httpServer, s2)
+        self.addLink(dnsServer, s2)
+        self.addLink(ntpServer, s2)
+        self.addLink(ftpServer, s2)
+
+
+topos = {
+    "secu": ( lambda: TopoSecu() )
+}
+attack_script = "attack_script.sh"
+
+
+def add_routes(net):
+    ### STATIC ROUTES ###
+    info(net['r1'].cmd("ip route add 10.2.0.0/24 via 10.12.0.2 dev r1-eth12"))
+    info(net['r2'].cmd("ip route add 10.1.0.0/24 via 10.12.0.1 dev r2-eth12"))
+    for h in ['http', 'dns', 'ntp', 'ftp']:
+        info(net[h].cmd("ip route replace 10.1.0.0/24 via 10.12.0.1"))
+
+def start_services(net: Mininet) -> None:
+    """
+    Start services on servers.
+    :param net: Mininet network
+    """
+    # Apache2 HTTP server
+    info(net['http'].cmd("/usr/sbin/apache2ctl -DFOREGROUND &"))
+    # dnsmasq DNS server
+    info(net['dns'].cmd("/usr/sbin/dnsmasq -k &"))
+    # NTP server
+    info(net['ntp'].cmd("/usr/sbin/ntpd -d &"))
+    # FTP server
+    info(net['ftp'].cmd("/usr/sbin/vsftpd &"))
+
+    # SSH server on all servers
+    cmd = "/usr/sbin/sshd -D &"
+    for host in ['http', 'ntp', 'ftp']:
+        info(net[host].cmd(cmd))
+
+def setup_firewall(net: Mininet) -> None:
+    #rules for dmz:
+    # 1. DMZ servers cannot send any ping or initiate any connection. 
+    # 2. They can only respond to incoming connections.
+    for h in ['http', 'dns', 'ntp', 'ftp']:
+        host = net[h]
+
+        host.cmd("nft flush ruleset")
+        host.cmd("nft add table inet filter")
+        # traffic entrant acepté par defaut
+        host.cmd("nft add chain inet filter input '{ type filter hook input priority 0; policy accept; }'")
+        # traffic sortant bloqué (cant init connection)
+        host.cmd("nft add chain inet filter output '{ type filter hook output priority 0; policy drop; }'")
+        # traffic local (lo) accepté, (jsp si c'est utile tbh)
+        host.cmd("nft add rule inet filter output oif lo accept")
+        # peut repondre a connection deja etablie
+        host.cmd("nft add rule inet filter output ct state established,related accept")
+
+    r1 = net['r1']
+    r1.cmd("nft flush ruleset")
+    r1.cmd("nft add table inet filter")
+    #cree chaine forward (traffic qui traverse rooter)
+    r1.cmd("nft add chain inet filter forward '{ type filter hook forward priority 0; policy drop; }'")
+    r1.cmd("nft add rule inet filter forward ct state invalid drop")
+    # peut repondre a connection deja etablie
+    r1.cmd("nft add rule inet filter forward ct state established,related accept")
+    #On autorise tout trafic qui vient de workstation
+    r1.cmd("nft add rule inet filter forward ip saddr 10.1.0.0/24 accept")
+
+    r2 = net['r2']
+    r2.cmd("nft flush ruleset")
+    r2.cmd("nft add table inet filter")
+    # par defaut on bloque tout
+    r2.cmd("nft add chain inet filter forward '{ type filter hook forward priority 0; policy drop; }'")
+
+    r2.cmd("nft add rule inet filter forward ct state invalid drop")
+    # peut repondre a connection deja etablie
+    r2.cmd("nft add rule inet filter forward ct state established,related accept")
+    # On autorise tout trafic qui vient de workstation
+    r2.cmd("nft add rule inet filter forward ip saddr 10.1.0.0/24 accept")
+    # autorise internet a ping uniquement les serveurs DMZ.
+    r2.cmd("nft add rule inet filter forward ip saddr 10.2.0.0/24 ip daddr { 10.12.0.10, 10.12.0.20, 10.12.0.30, 10.12.0.40 } accept")
+
+def stop_services(net: Mininet) -> None:
+    """
+    Stop services on servers.
+    :param net: Mininet network
+    """
+    # Apache2 HTTP server
+    info(net['http'].cmd("killall apache2"))
+    # dnsmasq DNS server
+    info(net['dns'].cmd("killall dnsmasq"))
+    # OpenNTPd NTP server
+    info(net['ntp'].cmd("killall ntpd"))
+    # FTP server
+    info(net['ftp'].cmd("killall vsftpd"))
+
+
+def delete_attack_script(attack_script: str) -> None:
+    """
+    Delete attack script.
+    :param attack_script: path to the attack script
+    """
+    try:
+        os.remove(attack_script)
+    except FileNotFoundError:
+        print(f"File {attack_script} does not exist.")
+
+
+def run():
+    topo = TopoSecu()
+    net = Mininet(topo=topo)
+
+    add_routes(net)
+    stop_services(net)
+    time.sleep(1)
+    start_services(net)
+    net.start()
+    setup_firewall(net)
+    CLI(net)
+    stop_services(net)
+    net.stop()
+
+    delete_attack_script(attack_script)
+
+
+def ping_all():
+    topo = TopoSecu()
+    net = Mininet(topo=topo)
+
+    add_routes(net)
+    stop_services(net)
+    start_services(net)
+
+    net.start()
+    net.pingAll()
+    stop_services(net)
+    net.stop()
+
+    delete_attack_script(attack_script)
+
+
+if __name__ == '__main__':
+
+
+    # Command-line arguments
+    parser = argparse.ArgumentParser(
+        prog="topo.py",
+        description="Mininet topology for the network attacks project of the course LINFO2347."
+    )
+
+    # Optional flag -p
+    parser.add_argument("-p", "--pingall", action="store_true", help="Perform pingall test")
+    # Parse arguments
+    args = parser.parse_args()
+
+    setLogLevel('info')
+
+    if args.pingall:
+        # Deploy topology, run pingall test, then exit
+        ping_all()
+    else:
+        # Deploy topology, open CLI
+        run()
